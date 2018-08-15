4809bc956a0c560eb53ec5bfd49684778614ad35
Mona Lisa <honterparade@gmail.com>
2018-08-15 14:26:51 +0200

    APP instances & testing

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
+const app = require('./lib/app.js');
 const dotenv = require('dotenv').config();
-const express = require('express');
-const app = express();
 
-app.get('/', ( req, res ) => {
-    res.send('Welcome to CentralRouter!');
-});
+/** Class representing the CentralRouter instance. */
+class App {
+    /**
+     * Create a instance.
+     * @param {String} appWebName 
+     * @param {Number} appWebPort 
+     * @param {String} appWssName 
+     * @param {Number} appWssPort 
+     * @param {String} appLocale 
+     */
+    constructor(appWebName, appWebPort, appWssName, appWssPort, appLocale) {
+        this.webName    = appWebName || "CentralRouter:WEB";
+        this.webPort    = appWebPort || 8080;
+        this.wssName    = appWssName || "CentralRouter:WSS";
+        this.wssPort    = appWssPort || 8081;
+        this.locale     = require(`./lib/translations/${appLocale}.json`) || require(`./lib/translations/en.json`);
+    }
 
-app.listen(process.env.APP_PORT, (err) => {
-    if(err) throw err;
+    /**
+     * App init method, depends on app-module.
+     */
+    init() {
+        module.exports = appInstance;
 
-    console.log(`${process.env.APP_NAME} started listening on port ${process.env.APP_PORT}!`);
-});
+        app.init_services();  
+    }
+}
+
+const appInstance = new App(
+    process.env.APP_NAME,
+    process.env.APP_PORT,
+    process.env.WSS_NAME,
+    process.env.WSS_PORT,
+    process.env.APP_LOCALE
+);
+appInstance.init();
--- test/app.endpoints.js
+++ test/app.endpoints.js
@@ -2,9 +2,17 @@ const express = require('express');
 const request = require('supertest');
 
 describe('app', () => {
-    describe('.endpoints', () => {
-        it('Should get welcome page: ', (done) => {
-            var app = express();
+    describe('.express:', () => {
+        it('Should start Express instance', (done) => {
+            const app = express();
+
+            if(app) done();
+        });
+    });
+
+    describe('.endpoints:', () => {
+        it('Should get welcome page', (done) => {
+            const app = express();
 
             app.get('/', function( req, res ) {
                 res.send('Welcome to CentralRouter!');
