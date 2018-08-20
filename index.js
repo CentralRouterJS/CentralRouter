a85d5718144c1befc7a6baf95d149ec54aff15e4
Mona Lisa <honterparade@gmail.com>
2018-08-20 17:09:44 +0200

    Database connection & interfaces

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const app = require('./lib/app.js');
+const expressService = require('./lib/app.js');
+const databaseService = require('./lib/database.js');
+const socketService = require('./lib/wss.js');
 const dotenv = require('dotenv').config();
 
 /** Class representing the CentralRouter instance. */
@@ -9,13 +11,24 @@ class App {
      * @param {Number} appWebPort 
      * @param {String} appWssName 
      * @param {Number} appWssPort 
+     * @param {String} appDBHost
+     * @param {Number} appDBPort
+     * @param {String} appDBName
+     * @param {String} appDBUser
+     * @param {String} appDBPass
      * @param {String} appLocale 
      */
-    constructor(appWebName, appWebPort, appWssName, appWssPort, appLocale) {
+    constructor(appWebName, appWebPort, appDomain, appWssName, appWssPort, appDBHost, appDBPort, appDBName, appDBUser, appDBPass, appLocale) {
         this.webName    = appWebName || "CentralRouter:WEB";
         this.webPort    = appWebPort || 8080;
+        this.webDomain  = appDomain  || "localhost";
         this.wssName    = appWssName || "CentralRouter:WSS";
         this.wssPort    = appWssPort || 8081;
+        this.dbHost     = appDBHost  || "localhost";
+        this.dbPort     = appDBPort  || 27017;
+        this.dbName     = appDBName  || "centralrouter";
+        this.dbUser     = appDBUser  || "";
+        this.dbPass     = appDBPass  || "";
         this.locale     = require(`./lib/translations/${appLocale}.json`) || require(`./lib/translations/en.json`);
     }
 
@@ -25,15 +38,23 @@ class App {
     init() {
         module.exports = appInstance;
 
-        app.init_services();  
+        expressService.init();
+        databaseService.init();
+        socketService.init();
     }
 }
 
 const appInstance = new App(
     process.env.APP_NAME,
     process.env.APP_PORT,
+    process.env.APP_DOMAIN,
     process.env.WSS_NAME,
     process.env.WSS_PORT,
+    process.env.DATABASE_HOST,
+    process.env.DATABASE_PORT,
+    process.env.DATABASE_NAME,
+    process.env.DATABASE_USER, 
+    process.env.DATABASE_PASS,
     process.env.APP_LOCALE
 );
 appInstance.init();
