d0d8cea657f03ed42ad372a8931f86ba87291f54
LeFizzy <honterparade@gmail.com>
2019-02-08 13:42:19 +0100

    Add IP validation.

--- test/middlewares.js
+++ test/middlewares.js
@@ -1,33 +1,40 @@
 const validate = require('../lib/middleware/validateServers');
 
 describe('middlewares', () => {
-    describe('validateServers', () => {
+    describe('Validate correct IPv4', () => {
         it('Should return with correct validation (IPv4):', (done) => {
             let validatedDataObject = validate.createServer('CentralRouterConnector', '127.0.0.1');
 
-            if( validatedDataObject.isValidAddress )
-                done();
-        }); 
-
-        it('Should return with correct validation (IPv6):', (done) => {
-            let validatedDataObject = validate.createServer('CentralRouterConnector', '0:0:0:0:0:0:0:1');
-
-            if( validatedDataObject.isValidAddress )
-                done();
-        });
-
-        it('Should return with invaild address (IPv4):', (done) => {
-            let validatedDataObject = validate.createServer('CentralRouterConnector', '512.255.255.0');
-
-            if( !(validatedDataObject.isValidAddress) )
-                done();
-        });
-
-        it('Should return with invaild address (IPv4)', (done) => {
-            let validatedDataObject = validate.createServer('CentralRouterConnector', 'Z:invaild:Z');
-
-            if( !(validatedDataObject.isValidAddress) )
+            if (validatedDataObject.isValidAddress)
                 done();
         });
     });
+    
+    describe('Validate correct IPv6', () => {
+        it('Should return with correct validation (IPv6):', (done) => {
+            let validatedDataObject = validate.createServer('CentralRouterConnector', '0:0:0:0:0:0:0:1');
+
+            if (validatedDataObject.isValidAddress)
+                done();
+        });
+    });
+
+    describe('Validate wrong IPv4', () => {
+        it('Should return with invaild address (IPv4):', (done) => {
+            let validatedDataObject = validate.createServer('CentralRouterConnector', '512.255.255.0');
+
+            if (!(validatedDataObject.isValidAddress))
+                done();
+        });
+    });
+
+    describe('Validate wrong IPv6', () => {
+        it('Should return with invaild address (IPv4)', (done) => {
+            let validatedDataObject = validate.createServer('CentralRouterConnector', 'Z:invaild:Z');
+
+            if (!(validatedDataObject.isValidAddress))
+                done();
+        });
+    });
+
 });
